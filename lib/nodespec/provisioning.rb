4cad54963541451932c4008d9ece87ae7bd4a403
Silvio Montanari <montanari.silvio@gmail.com>
2014-06-23 09:23:00 +0100

    instantiate provisioner only once per example group

--- lib/nodespec/provisioning.rb
+++ lib/nodespec/provisioning.rb
@@ -3,9 +3,10 @@ Dir[File.join(File.dirname(__FILE__), 'provisioning/*.rb')].each {|f| require f}
 module NodeSpec
   module Provisioning
     self.constants.each do |provisioner_name|
-      provisioner = self.const_get(provisioner_name)
+      provisioner_class = self.const_get(provisioner_name)
       define_method("node_provision_with_#{provisioner_name.downcase}".to_sym) do |*args, &block|
-        provisioner.new(*args).instance_eval(&block)
+        @provisioner ||= provisioner_class.new(*args)
+        @provisioner.instance_eval(&block)
       end
     end
   end
--- spec/provisioning_spec.rb
+++ spec/provisioning_spec.rb
@@ -4,19 +4,27 @@ module NodeSpec
   describe Provisioning do
     let(:subject) {Object.new.extend Provisioning}
     let(:provisioner_instance){double('provisioner instance')}
-    provisioners = {
+    let(:provisioning_block) {Proc.new {}}
+    
+    {
       'puppet' => Provisioning::Puppet,
       'chef' => Provisioning::Chef
-    }
-    provisioners.each do |name, clazz|
+    }.each do |name, clazz|
+      it "instantiate the #{clazz} provisioner once only" do
+        expect(clazz).to receive(:new).once.and_return(provisioner_instance)
+        expect(provisioner_instance).to receive(:instance_eval).twice
+
+        subject.send("node_provision_with_#{name}", &provisioning_block)
+        subject.send("node_provision_with_#{name}", &provisioning_block)
+      end
+
       it "executes the #{clazz} command" do
-        block = Proc.new {}
-        expect(clazz).to receive(:new).with('foo', 'bar').and_return(provisioner_instance)
+        expect(clazz).to receive(:new).and_return(provisioner_instance)
         expect(provisioner_instance).to receive(:instance_eval) do |&b|
-          expect(b).to be block
+          expect(b).to be provisioning_block
         end
 
-        subject.send("node_provision_with_#{name}", 'foo', 'bar', &block)
+        subject.send("node_provision_with_#{name}", &provisioning_block)
       end
     end
   end
